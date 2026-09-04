c547d495efe23e44cdea64314330b5f537407fd1
alinezaar53-rgb <alinezaar53@gmail.com>
2026-09-04 07:53:01 -0700

    feat: initialize app structure with home screen and custom theme constants

---
+++ lib/screens/HomePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hw/themes/AppColor.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: backgroundColor,
+      child: Column(
+        children: const[
+          Text("Hello Word"),
+          Text("Hello Ali"),
+          ElevatedButton(onPressed: null, child: Text("Button"),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
---
+++ lib/themes/AppColor.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xFF30345B);
